cd3f9341128c3af9cd42f1b00f6ba5951b4ecfc4
emuhlestein <emuhlestein@yahoo.com>
2020-02-29 07:46:26 -0600

    Add Class Component

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Ed</h1>
+            </div>
+        )
+    }
+}
+
+export default Hello
